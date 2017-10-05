1be8bcfb2f99849696064083413497a28a062a02
marikh <marikh@codevalue.net>
2017-10-05 22:12:55 +0300

    login reducer tests

--- src/pages/Login/loginReducer.test.js
+++ src/pages/Login/loginReducer.test.js
@@ -1,159 +1,120 @@
-// import loginReducer, {getUserNameSelector, getPasswordSelector, 
-//     getLoggingInSelector, getLoggedInSelector, getLoginFailedSelector}  from './loginReducer';
+import loginReducer, {getUserNameSelector, getPasswordSelector, 
+    getLoggingInSelector, getLoggedInSelector, getLoginFailedSelector}  from './loginReducer';
 
-// import { actionTypes } from './actions'
-// import { globalActions } from '../../common/globalActions'
+import { actionTypes } from './actions'
+import { globalActions } from '../../common/globalActions'
 
 
-// const TEST_INITIAL_STATE = {
-//   userName : "",
-//   password : "",
-//   loggingIn : false,
-//   loggedIn : false,
-//   loginFailed : false,
-// };
+const TEST_INITIAL_STATE = {
+  userName : "",
+  password : "",
+  loggingIn : false,
+  loggedIn : false,
+  loginFailed : false,
+};
 
-// describe('loginReducer test', () => {
-//     it('should return proper initial value', () => {
-//         expect(loginReducer(undefined, {})).toEqual(TEST_INITIAL_STATE)
-//     })
+describe('loginReducer test', () => {
+    it('should return proper initial value', () => {
+        expect(loginReducer(undefined, {})).toEqual(TEST_INITIAL_STATE)
+    })
 
-//     it('updateFieldAction - updates "userName" field', () => {
+    it('updateFieldAction - updates "userName" field', () => {
         
-//         const updateFieldAction = {
-//             type: actionTypes.CONTACT_FIELD_CHANGED,
-//             changedFieldName: "userName",
-//             newValue: "Alti Zahen"
-//         }
+        const updateFieldAction = {
+            type: actionTypes.LOGIN_FIELD_CHANGED,
+            changedFieldName: "userName",
+            newValue: "Alti Zahen"
+        }
 
-//         expect(loginReducer(TEST_INITIAL_STATE, updateFieldAction))
-//         .toEqual({
-//             ...TEST_INITIAL_STATE,
-//             name: "Alti Zahen"
-//         })  
-//     })
+        expect(loginReducer(TEST_INITIAL_STATE, updateFieldAction))
+        .toEqual({
+            ...TEST_INITIAL_STATE,
+            userName: "Alti Zahen"
+        })  
+    })
     
-//     it('loginReducer - updates "password" field', () => {
+    it('loginReducer - updates "password" field', () => {
         
-//         const updateFieldAction = {
-//             type: actionTypes.CONTACT_FIELD_CHANGED,
-//             changedFieldName: "password",
-//             newValue: "altigmailcom"
-//         }
+        const updateFieldAction = {
+            type: actionTypes.LOGIN_FIELD_CHANGED,
+            changedFieldName: "password",
+            newValue: "altigmailcom"
+        }
 
-//         expect(loginReducer(TEST_INITIAL_STATE, updateFieldAction))
-//         .toEqual({
-//             ...TEST_INITIAL_STATE,
-//             email: "altigmailcom"
-//         })  
-//     })
+        expect(loginReducer(TEST_INITIAL_STATE, updateFieldAction))
+        .toEqual({
+            ...TEST_INITIAL_STATE,
+            password: "altigmailcom"
+        })  
+    })
 
-    
-//     it('updateFieldAction - updates "message" field', () => {
+    it('userLoggingInAction', () => {
         
-//         const updateFieldAction = {
-//             type: actionTypes.CONTACT_FIELD_CHANGED,
-//             changedFieldName: "message",
-//             newValue: "Aron, shulhan, kisaot, mehonat kvisa. alti zahen alti zahen."
-//         }
+        var dirtyState = { ...TEST_INITIAL_STATE,
+            userName : "s",
+            password : "s@l.com"};
 
-//         expect(contactReducer(TEST_INITIAL_STATE, updateFieldAction))
-//         .toEqual({
-//             ...TEST_INITIAL_STATE,
-//             message: "Aron, shulhan, kisaot, mehonat kvisa. alti zahen alti zahen."
-//         })  
-//     })
+        expect(loginReducer(dirtyState, {type: actionTypes.USER_LOGGING_IN}))
+        .toEqual({
+            ...dirtyState,
+            loggingIn : true,
+            loggedIn: false,
+            loginFailed : false
+        })  
+    })
 
-//     it('updateFieldAction - updates "name" field', () => {
+    it('userLoggedInAction', () => {
         
-//         var failedState = { ...TEST_INITIAL_STATE, submitContactFailed: true };
+        var dirtyState = { ...TEST_INITIAL_STATE,
+            userName : "s",
+            password : "s@l.com"};
 
-//         const updateFieldAction = {
-//             type: actionTypes.CONTACT_FIELD_CHANGED,
-//             changedFieldName: "name",
-//             newValue: "Alti Zahe"
-//         }
+        expect(loginReducer(dirtyState, {type: globalActions.USER_LOGGED_IN}))
+        .toEqual({
+            ...dirtyState,
+            loggingIn : false,
+            loggedIn: true,
+            loginFailed : false,
+        })  
+    })
 
-//         expect(contactReducer(failedState, updateFieldAction))
-//         .toEqual({
-//             ...TEST_INITIAL_STATE,
-//             name: "Alti Zahe",
-//             submitContactFailed : false
-//         })  
-//     })
-
-
-//     it('contactFormSubmittedAction', () => {
+    it('loginFailedAction', () => {
         
-//         var dirtyState = {
-//             name : "s",
-//             email : "s@c.com",
-//             message : "s", 
-//             submitContactFailed: true };
+        var dirtyState = { ...TEST_INITIAL_STATE,
+            userName : "s",
+            password : "s@l.com"};
 
-//         expect(contactReducer(dirtyState, {type: actionTypes.CONTACT_FORM_SUBMITTED}))
-//         .toEqual({
-//             ...dirtyState,
-//             name : "",
-//             email : "",
-//             message : "",
-//             submitContactFailed: false,
-//         })  
-//     })
+        expect(loginReducer(dirtyState, {type: actionTypes.LOGIN_FAILED}))
+        .toEqual({
+            ...dirtyState,
+            loggingIn : false,
+            loggedIn: false,
+            loginFailed : true,
+        })  
+    })
 
- 
-//     it('updateFieldAction action - updates "name" field', () => {
-        
-//         var dirtyState = {
-//             name : "s",
-//             email : "s@.com",
-//             message : "s", 
-//             submitContactFailed: false };
+    it('getUserNameSelector', () => {
+        const state = { loginPage: { ...TEST_INITIAL_STATE, userName: "da" } };
+        expect(getUserNameSelector(state)).toEqual("da")
+    })
 
-//         expect(contactReducer(dirtyState, {type: actionTypes.FAILED_SUBMITTING}))
-//         .toEqual({
-//             ...dirtyState,
-//             submitContactFailed: true
-//         })  
-//     })
+    it('getPasswordSelector', () => {
+        const state = { loginPage: { ...TEST_INITIAL_STATE, password: "dacom" } };
+        expect(getPasswordSelector(state)).toEqual("dacom")
+    })
 
-//     it('updateFieldAction action - updates "name" field', () => {
-        
-//         var dirtyState = { 
-//             name : "s",
-//             email : "s@.com",
-//             message : "s", 
-//             submitContactFailed: false };
+    it('getLoggingInSelector', () => {
+        const state = { loginPage: { ...TEST_INITIAL_STATE, loggingIn: true } };
+        expect(getLoggingInSelector(state)).toEqual(true)
+    })
 
-//         expect(contactReducer(dirtyState, {type: actionTypes.UNLOAD_CONTACT_FORM}))
-//         .toEqual({
-//             ...dirtyState,
-//             ...TEST_INITIAL_STATE
-//         })  
-//     })
+    it('getLoggedInSelector', () => {
+        const state = { loginPage: { ...TEST_INITIAL_STATE, loggedIn: true } };
+        expect(getLoggedInSelector(state)).toEqual(true)
+    })
 
-//     it('getUserNameSelector', () => {
-//         const state = { loginPage: { ...TEST_INITIAL_STATE, userName: "da" } };
-//         expect(getUserNameSelector(state)).toEqual("da")
-//     })
-
-//     it('getPasswordSelector', () => {
-//         const state = { loginPage: { ...TEST_INITIAL_STATE, password: "dacom" } };
-//         expect(getPasswordSelector(state)).toEqual("dacom")
-//     })
-
-//     it('getLoggingInSelector', () => {
-//         const state = { loginPage: { ...TEST_INITIAL_STATE, loggingIn: true } };
-//         expect(getLoggingInSelector(state)).toEqual(true)
-//     })
-
-//     it('getLoggedInSelector', () => {
-//         const state = { loginPage: { ...TEST_INITIAL_STATE, loggedIn: true } };
-//         expect(getLoggedInSelector(state)).toEqual(true)
-//     })
-
-//     it('getLoginFailedSelector', () => {
-//         const state = { loginPage: { ...TEST_INITIAL_STATE, loginFailed: true } };
-//         expect(getLoginFailedSelector(state)).toEqual(true)
-//     })
-// })
+    it('getLoginFailedSelector', () => {
+        const state = { loginPage: { ...TEST_INITIAL_STATE, loginFailed: true } };
+        expect(getLoginFailedSelector(state)).toEqual(true)
+    })
+})
